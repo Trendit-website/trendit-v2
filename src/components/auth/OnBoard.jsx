231548f02e1ed5063c5efaf2f88a591a28658a6c
osomhe1 <osomhe.aleogho@gmail.com>
2024-04-04 17:45:24 +0100

    update

--- src/components/auth/OnBoard.jsx
+++ src/components/auth/OnBoard.jsx
@@ -85,62 +85,9 @@ export default function OnBoard() {
     }
   }
 
-  const animals = [
-    {
-      label: 'Cat',
-      value: 'cat',
-      description: 'The second most popular pet in the world',
-    },
-    {
-      label: 'Dog',
-      value: 'dog',
-      description: 'The most popular pet in the world',
-    },
-    {
-      label: 'Elephant',
-      value: 'elephant',
-      description: 'The largest land animal',
-    },
-    { label: 'Lion', value: 'lion', description: 'The king of the jungle' },
-    { label: 'Tiger', value: 'tiger', description: 'The largest cat species' },
-    {
-      label: 'Giraffe',
-      value: 'giraffe',
-      description: 'The tallest land animal',
-    },
-    {
-      label: 'Dolphin',
-      value: 'dolphin',
-      description: 'A widely distributed and diverse group of aquatic mammals',
-    },
-    {
-      label: 'Penguin',
-      value: 'penguin',
-      description: 'A group of aquatic flightless birds',
-    },
-    {
-      label: 'Zebra',
-      value: 'zebra',
-      description: 'A several species of African equids',
-    },
-    {
-      label: 'Shark',
-      value: 'shark',
-      description:
-        'A group of elasmobranch fish characterized by a cartilaginous skeleton',
-    },
-  ]
-
   return (
     <>
       <div>
-        <Select label='Select an animal' className='max-w-xs'>
-          {animals.map((animal) => (
-            <SelectItem key={animal.value} value={animal.value}>
-              {animal.label}
-            </SelectItem>
-          ))}
-        </Select>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className=' h[1024px] h-[1054px] relative bg-white dark:bg-black'>
             <div className='left-0 top-0 absolute'>
